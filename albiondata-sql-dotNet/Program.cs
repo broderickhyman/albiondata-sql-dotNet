dfd93a6c71b12e91988e8a21553fa5ccf1aa79c9
broderick.hyman <broderick.hyman@gentex.com>
2018-08-30 12:29:38 -0400

    Add project files.

---
+++ albiondata-sql-dotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace albiondata_sql_dotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
